1fa35226a0d69a22fa3828e1533fa0cdfb3c03a5
Travis Thieman <travis.thieman@gmail.com>
2016-02-01 14:21:20 -0500

    Drop Python 3 support because the Avro library is awful

--- setup.py
+++ setup.py
@@ -5,14 +5,9 @@ from kafka_rest import __version__
 
 install_requires = [
     'tornado>=4.0.0,<5.0.0',
-    'avro_json_serializer>=0.4.1,<0.5.0'
+    'avro_json_serializer>=0.4.1,<0.5.0',
+    'avro==1.7.7'
 ]
-
-# TODO: Be more liberal with Avro versions
-if sys.version_info[0] >= 3:
-    install_requires.append('avro-python3==1.7.7')
-else:
-    install_requires.append('avro==1.7.7')
 
 setup(
     name='kafka-rest',
